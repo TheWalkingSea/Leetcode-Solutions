2df82d4a90cc2d1812b3058835d1481e5d56da02
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2025-10-13 09:31:42 -0400

    Added question 37 - Sudoku Solver

--- Code/23.merge-k-sorted-lists.java
+++ Code/23.merge-k-sorted-lists.java
@@ -1,52 +1,59 @@
-# Category: algorithms
-# Level: Hard
-# Percent: 57.84542%
+// Category: algorithms
+// Level: Hard
+// Percent: 57.84542%
 
 
 
-# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-# 
-# Merge all the linked-lists into one sorted linked-list and return it.
-# 
-#  
-# Example 1:
-# 
-# Input: lists = [[1,4,5],[1,3,4],[2,6]]
-# Output: [1,1,2,3,4,4,5,6]
-# Explanation: The linked-lists are:
-# [
-#   1->4->5,
-#   1->3->4,
-#   2->6
-# ]
-# merging them into one sorted linked list:
-# 1->1->2->3->4->4->5->6
-# 
-# 
-# Example 2:
-# 
-# Input: lists = []
-# Output: []
-# 
-# 
-# Example 3:
-# 
-# Input: lists = [[]]
-# Output: []
-# 
-# 
-#  
-# Constraints:
-# 
-# 
-# 	k == lists.length
-# 	0 <= k <= 10⁴
-# 	0 <= lists[i].length <= 500
-# 	-10⁴ <= lists[i][j] <= 10⁴
-# 	lists[i] is sorted in ascending order.
-# 	The sum of lists[i].length will not exceed 10⁴.
-# 
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+// 
+// Merge all the linked-lists into one sorted linked-list and return it.
+//
+//  
+// Example 1:
+// 
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted linked list:
+// 1->1->2->3->4->4->5->6
+// 
+// 
+// Example 2:
+// 
+// Input: lists = []
+// Output: []
+// 
+// 
+// Example 3:
+// 
+// Input: lists = [[]]
+// Output: []
+// 
+// 
+//  
+// Constraints:
+// 
+// 
+// 	k == lists.length
+// 	0 <= k <= 10⁴
+// 	0 <= lists[i].length <= 500
+// 	-10⁴ <= lists[i][j] <= 10⁴
+// 	lists[i] is sorted in ascending order.
+// 	The sum of lists[i].length will not exceed 10⁴.
+// 
  
+
+// public class ListNode {
+//      int val;
+//      ListNode next;
+//      ListNode() {}
+//      ListNode(int val) { this.val = val; }
+//      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
 /**
  * Definition for singly-linked list.
@@ -59,7 +66,33 @@
  * }
  */
 class Solution {
+
     public ListNode mergeKLists(ListNode[] lists) {
-        
+        ListNode head = null;
+        ListNode curr = null;
+
+        while (true) {
+            Integer minIndex = null;
+            for (int i = 0; i < lists.length; i++) {
+                if (lists[i] != null && (minIndex == null || lists[i].val < lists[minIndex].val)) {
+                    minIndex = i;
+                }
+            }
+
+            if (minIndex == null) {
+                return head;
+            }
+
+            if (head == null) {
+                head = new ListNode(lists[minIndex].val);
+                curr = head;
+            } else {
+                ListNode newNode = new ListNode(lists[minIndex].val);
+                curr.next = newNode;
+                curr = newNode;
+            }
+
+            lists[minIndex] = lists[minIndex].next;
+        }
     }
 }
